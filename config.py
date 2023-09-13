beb190f206afb5b7f611716b5ff16ab80cacb8b3
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2023-09-13 06:24:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 HANDLER = ["~", ".","!","?","@","$"]
 GROUP_ID = os.getenv("GROUP_ID")
-SOURCE = "https://github.com/TeamX-Coders/PyroX-UB"
+SOURCE = "https://github.com/Darklightning2008/PyroX" 
 LOG_CHAT = os.getenv("LOG_CHAT")
+BOTUSERNAME = os.getenv("BOTUSERNAME")
